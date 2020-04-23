b9d03f4a07bb862dcbd086184d19db52419ed591
zyh940305858 <940305858@qq.com>
2020-04-23 22:07:02 +0800

    Finish add article api

--- app/controller/admin/main.js
+++ app/controller/admin/main.js
@@ -26,6 +26,18 @@ class MainController extends Controller {
     this.ctx.body = { data: resType };
   }
 
+  async addArticle() {
+    const tmpArticle = this.ctx.request.body;
+    const result = await this.app.mysql.insert('article', tmpArticle);
+    const insertSuccess = result.affectedRows === 1;
+    const insertId = result.insertId;
+
+    this.ctx.body = {
+      isSuccess: insertSuccess,
+      insertId,
+    };
+  }
+
 }
 
 module.exports = MainController;
