f4c7b33d4ed2bf19a0ecf1c3ae00c88abeb0836b
zyh940305858 <940305858@qq.com>
2020-05-03 16:51:42 +0800

    Finish artilce list api

--- app/controller/admin/main.js
+++ app/controller/admin/main.js
@@ -8,6 +8,7 @@ class MainController extends Controller {
     this.ctx.body = 'hi,api';
   }
 
+  // 登录接口
   async checkLogin() {
     const { userName, password } = this.ctx.request.body;
     const sql = `SELECT userName FROM admin_user WHERE userName = "${userName}" AND password="${password}"`;
@@ -21,11 +22,13 @@ class MainController extends Controller {
     }
   }
 
+  // 获取文章类型接口
   async getTypeInfo() {
     const resType = await this.app.mysql.select('type');
     this.ctx.body = { data: resType };
   }
 
+  // 添加文章接口
   async addArticle() {
     const tmpArticle = this.ctx.request.body;
     const result = await this.app.mysql.insert('article', tmpArticle);
@@ -38,14 +41,28 @@ class MainController extends Controller {
     };
   }
 
+  // 更新文章接口
   async updateArticle() {
     const tempArticle = this.ctx.request.body;
     const result = await this.app.mysql.update('article', tempArticle);
     const updateSuccess = result.affectedRows === 1;
 
-    this.ctx.body = {
-      isSuccess: updateSuccess,
-    };
+    this.ctx.body = { isSuccess: updateSuccess };
+  }
+
+  // 获取文章列表接口
+  async getArticleList() {
+    const sql = 'SELECT article.id as id ,' +
+      'article.title as title ,' +
+      'article.introduce as introduce ,' +
+      // "FROM_UNIXTIME(article.addTime,'%Y-%m-%d %H:%i:%s') as addTime"+
+      'article.addTime as addTime ,' +
+      'article.view_count as view_count ,' +
+      'type.typeName as typeName ' +
+      'FROM article LEFT JOIN type ON article.type_id = type.Id ' +
+      'ORDER BY article.id DESC';
+    const resList = await this.app.mysql.query(sql);
+    this.ctx.body = { list: resList };
   }
 
 }
--- app/router/admin.js
+++ app/router/admin.js
@@ -9,4 +9,5 @@ module.exports = app => {
   router.get('/admin/getTypeInfo', adminauth, controller.admin.main.getTypeInfo);
   router.post('/admin/addArticle', adminauth, controller.admin.main.addArticle);
   router.post('/admin/updateArticle', adminauth, controller.admin.main.updateArticle);
+  router.get('/admin/getArticleList', adminauth, controller.admin.main.getArticleList);
 };
